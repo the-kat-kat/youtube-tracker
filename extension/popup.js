855f99f12a33829b47c6f1d8da37c487b704cee5
katiikati <166103545+the-kat-kat@users.noreply.github.com>
2026-06-12 19:42:45 -0700

    change popup font size

--- extension/popup.js
+++ extension/popup.js
@@ -4,5 +4,5 @@ fetch("https://youtube-tracker-production-2dbe.up.railway.app/daily")
             const seconds = days[0]?.total_seconds || 0;
             const m = Math.floor(seconds / 60);
             const s = seconds % 60;
-            document.getElementById("today-time").textContent = `Today: ${m}m ${s}s watched`;
+            document.getElementById("today-time").textContent = `${m}m ${s}s watched`;
 });
